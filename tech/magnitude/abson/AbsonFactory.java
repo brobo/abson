70c6017f0abc03f7a652e3ef24f312eb92876412
Colby Brown <cbrownlinux@gmail.com>
2014-05-29 20:38:31 -0500

    AbsonFactories are no longer singletons because that was a silly idea from the start.

--- tech/magnitude/abson/AbsonFactory.java
+++ tech/magnitude/abson/AbsonFactory.java
@@ -7,11 +7,6 @@ import tech.magnitude.abson.elements.AbsonObject;
 
 public class AbsonFactory {
 	
-	private static final AbsonFactory singleton = new AbsonFactory();
-	
-	private static AbsonFactory getSingleton() {
-		return singleton;
-	}
 	
 	private Map<Class<? extends AbsonDecomposable>, AbsonComposer<? extends AbsonDecomposable>> composers;
 	private Map<String, Class<? extends AbsonDecomposable>> classNames;
@@ -21,21 +16,23 @@ public class AbsonFactory {
 		classNames = new HashMap<>();
 	}
 	
-	public static <T extends AbsonDecomposable> void registerComposer(Class<T> _class, AbsonComposer<T> composer) {
-		AbsonFactory factory = getSingleton();
-		factory.classNames.put(_class.getName(), _class);
-		factory.composers.put(_class, composer);
+	public <T extends AbsonDecomposable> void registerComposer(String key, Class<T> _class, AbsonComposer<T> composer) {
+		classNames.put(key, _class);
+		registerComposer(_class, composer);
+	}
+	
+	public <T extends AbsonDecomposable> void registerComposer(Class<T> _class, AbsonComposer<T> composer) {
+		classNames.put(_class.getName(), _class);
+		composers.put(_class, composer);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static <T extends AbsonDecomposable> T compose(Class<T> _class, AbsonObject object) {
-		AbsonFactory factory = getSingleton();
-		return (T)factory.composers.get(_class).compose(object);
+	public <T extends AbsonDecomposable> T compose(Class<T> _class, AbsonObject object) {
+		return (T)composers.get(_class).compose(object);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static <T extends AbsonDecomposable> T compose(String className, AbsonObject object) {
-		AbsonFactory factory = getSingleton();
-		return (T)compose(factory.classNames.get(className), object);
+	public <T extends AbsonDecomposable> T compose(String className, AbsonObject object) {
+		return (T)compose(classNames.get(className), object);
 	}
 }
